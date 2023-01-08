4431d93c2121c170b16c70e5bc028495881a3dd6
SamuGG <SamuGG@users.noreply.github.com>
2023-01-08 20:03:24 +0000

    chore: Create chapter 2 exercises test project

---
+++ tests/chapter-02/Chapter02Tests.cs
@@ -0,0 +1,12 @@
+using FunctionalProgramming.Exercises.Chapter02;
+
+namespace FunctionalProgramming.Exercises.Tests;
+
+public class Chapter02Tests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
