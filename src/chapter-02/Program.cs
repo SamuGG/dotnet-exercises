d955f14ed2c1d7c91a8ad878bc9098f9ed3e507c
SamuGG <SamuGG@users.noreply.github.com>
2023-01-08 19:46:34 +0000

    feat: Calculate BMI

--- src/chapter-02/Program.cs
+++ src/chapter-02/Program.cs
@@ -1,10 +1,41 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿namespace FunctionalProgramming.Exercises.Chapter02;
 
 internal sealed class Program
 {
-    [SuppressMessage("Globalization", "CA1303: Do not pass literals as localized parameters")]
     private static void Main(string[] _)
     {
-        Console.WriteLine("Hello, World!");
+        double height = ReadDouble("Enter height in meters: ");
+        double weight = ReadDouble("Enter weight in kilograms: ");
+        double relation = CalculateBMI(height, weight);
+        Console.WriteLine(ToBmiRange(relation));
+    }
+
+    private static double ReadDouble(string message)
+    {
+        Console.Write(message);
+        _ = double.TryParse(Console.ReadLine(), out double value);
+        return value;
+    }
+
+    private static double CalculateBMI(double height, double weight)
+    {
+        return Math.Round(weight / Math.Pow(height, 2), 2);
+    }
+
+    private static BmiRange ToBmiRange(double relation)
+    {
+        return relation switch
+        {
+            < 18.5 => BmiRange.Underweight,
+            >= 25 => BmiRange.Overweight,
+            _ => BmiRange.Healthy
+        };
+    }
+
+    public enum BmiRange
+    {
+        Underweight,
+        Healthy,
+        Overweight
     }
 }
