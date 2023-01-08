36832fe0167100882a3c7b6a9083e2f833d21aa0
SamuGG <SamuGG@users.noreply.github.com>
2023-01-08 17:23:58 +0000

    feat: Quicksort function

--- src/Solutions.cs
+++ src/Solutions.cs
@@ -1,4 +1,6 @@
-﻿namespace FunctionalProgramming.Exercises.Chapter01;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FunctionalProgramming.Exercises.Chapter01;
 
 public static class Solutions
 {
@@ -6,5 +8,23 @@ public static class Solutions
     {
         ArgumentNullException.ThrowIfNull(predicate);
         return !predicate(obj);
+    }
+
+    [SuppressMessage("Design", "CA1002: Do not expose generic lists")]
+    public static List<int> Quicksort(this List<int> list)
+    {
+        ArgumentNullException.ThrowIfNull(list);
+
+        if (list.Count == 0)
+            return list;
+
+        var pivot = list.First();
+        var lowerElements = list.Skip(1).Where(x => x <= pivot).ToList();
+        var higherElements = list.Skip(1).Where(x => pivot < x).ToList().Quicksort();
+
+        return lowerElements.Quicksort()
+            .Append(pivot)
+            .Concat(higherElements.Quicksort())
+            .ToList();
     }
 }
