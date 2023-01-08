6dfb20f78f43f717f4755cd712f1909cd6c3837e
SamuGG <SamuGG@users.noreply.github.com>
2023-01-08 19:07:10 +0000

    chore: Create chapter 2 exercise project

---
+++ src/chapter-02/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+internal sealed class Program
+{
+    [SuppressMessage("Globalization", "CA1303: Do not pass literals as localized parameters")]
+    private static void Main(string[] _)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
