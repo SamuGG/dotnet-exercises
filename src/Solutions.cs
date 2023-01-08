3c657e1d0203ebaea2fe488e0f583e08aa8a92a5
SamuGG <SamuGG@users.noreply.github.com>
2023-01-08 17:09:56 +0000

    feat: Negate predicate function

--- src/Solutions.cs
+++ src/Solutions.cs
@@ -1,4 +1,10 @@
 ﻿namespace FunctionalProgramming.Exercises.Chapter01;
 
 public static class Solutions
-{ }
+{
+    public static bool Negate<T>(this Func<T, bool> predicate, T obj)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+        return !predicate(obj);
+    }
+}
