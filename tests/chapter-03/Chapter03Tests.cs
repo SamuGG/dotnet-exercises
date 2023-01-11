bebe623491be501a27318e04790564fbd4b1b775
SamuGG <SamuGG@users.noreply.github.com>
2023-01-11 20:23:12 +0000

    test: Lookup function tests

--- tests/chapter-03/Chapter03Tests.cs
+++ tests/chapter-03/Chapter03Tests.cs
@@ -18,4 +18,18 @@ public class Chapter03Tests
         var actual = value.Parse2<DayOfWeek>();
         Assert.Equal(possibleDay, actual);
     }
+
+    public static TheoryData<IEnumerable<int>, int, Option<int>> LookupTestData => new()
+    {
+        { new List<int> { 1, 2, 3 }, 4, F.None },
+        { new List<int> { 1, 2, 3 }, 2, F.Some(2) }
+    };
+
+    [Theory]
+    [MemberData(nameof(LookupTestData))]
+    public void LookupReturnsOption(IEnumerable<int> haystack, int needle, Option<int> expected)
+    {
+        var actual = haystack.Lookup(x => x == needle);
+        Assert.Equal(expected, actual);
+    }
 }
