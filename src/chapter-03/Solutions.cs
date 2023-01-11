01df4a45df289c7c453e2c8775b504b45d643d4e
SamuGG <SamuGG@users.noreply.github.com>
2023-01-11 20:03:55 +0000

    feat: Enum parser

--- src/chapter-03/Solutions.cs
+++ src/chapter-03/Solutions.cs
@@ -1,5 +1,12 @@
-﻿namespace FunctionalProgramming.Exercises.Chapter01;
+using LaYumba.Functional;
+using static LaYumba.Functional.F;
+
+namespace FunctionalProgramming.Exercises.Chapter03;
 
 public static class Solutions
 {
+    public static Option<T> Parse2<T>(this string value) where T : struct
+    {
+        return System.Enum.TryParse(value, out T converted) ? Some(converted) : None;
+    }
 }
