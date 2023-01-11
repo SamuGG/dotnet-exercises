0376c55a2434c41e1cadc00a322dd60b798b48b2
SamuGG <SamuGG@users.noreply.github.com>
2023-01-11 21:07:22 +0000

    test: Config map tests

--- tests/chapter-03/Chapter03Tests.cs
+++ tests/chapter-03/Chapter03Tests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Specialized;
 using FunctionalProgramming.Exercises.Chapter03;
 using LaYumba.Functional;
 
@@ -13,7 +14,7 @@ public class Chapter03Tests
 
     [Theory]
     [MemberData(nameof(ParseTestData))]
-    public void ParseStringReturnsOption(string value, Option<DayOfWeek> possibleDay)
+    public void ParseStringReturnsPossibleEnum(string value, Option<DayOfWeek> possibleDay)
     {
         var actual = value.Parse2<DayOfWeek>();
         Assert.Equal(possibleDay, actual);
@@ -27,9 +28,39 @@ public class Chapter03Tests
 
     [Theory]
     [MemberData(nameof(LookupTestData))]
-    public void LookupReturnsOption(IEnumerable<int> haystack, int needle, Option<int> expected)
+    public void LookupReturnsPossibleValue(IEnumerable<int> haystack, int needle, Option<int> expected)
     {
         var actual = haystack.Lookup(x => x == needle);
+        Assert.Equal(expected, actual);
+    }
+
+    public static TheoryData<NameValueCollection, string, Option<int>> ConfigOptionalTestData => new()
+    {
+        { new NameValueCollection { ["key4"] = "-123" }, "key4", F.Some(-123) },
+        { new NameValueCollection(), "key4", F.None }
+    };
+
+    [Theory]
+    [MemberData(nameof(ConfigOptionalTestData))]
+    public void GetPossibleMappedValue(NameValueCollection map, string key, Option<int> expected)
+    {
+        var sut = new AppConfig(map);
+        var actual = sut.Get<int>(key);
+        Assert.Equal(expected, actual);
+    }
+
+    public static TheoryData<NameValueCollection, string, int, int> ConfigStronglyTypedTestData => new()
+    {
+        { new NameValueCollection { ["key5"] = "880" }, "key5", 5, 880 },
+        { new NameValueCollection(), "key5", 71, 71 }
+    };
+
+    [Theory]
+    [MemberData(nameof(ConfigStronglyTypedTestData))]
+    public void GetStronglyTypedMappedValue(NameValueCollection map, string key, int defaultValue, int expected)
+    {
+        var sut = new AppConfig(map);
+        int actual = sut.Get(key, defaultValue);
         Assert.Equal(expected, actual);
     }
 }
