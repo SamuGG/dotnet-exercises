3c3d15d34524b9ad99db605af0bd4d335ca18d0c
SamuGG <SamuGG@users.noreply.github.com>
2023-01-11 20:21:41 +0000

    feat: Lookup

--- src/chapter-03/Solutions.cs
+++ src/chapter-03/Solutions.cs
@@ -1,4 +1,4 @@
-using LaYumba.Functional;
+﻿using LaYumba.Functional;
 using static LaYumba.Functional.F;
 
 namespace FunctionalProgramming.Exercises.Chapter03;
@@ -8,5 +8,17 @@ public static class Solutions
     public static Option<T> Parse2<T>(this string value) where T : struct
     {
         return System.Enum.TryParse(value, out T converted) ? Some(converted) : None;
+    }
+
+    public static Option<T> Lookup<T>(this IEnumerable<T> enumerable, Func<T, bool> predicate)
+    {
+        ArgumentNullException.ThrowIfNull(enumerable);
+        ArgumentNullException.ThrowIfNull(predicate);
+
+        foreach (T element in enumerable)
+            if (predicate(element))
+                return Some(element);
+
+        return None;
     }
 }
