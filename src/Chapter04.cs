4720157aa44dd1de3e7b3efca6f6442397e6fd50
SamuGG <SamuGG@users.noreply.github.com>
2023-01-18 19:47:15 +0000

    feat: Map Option and IEnumerable

--- src/Chapter04.cs
+++ src/Chapter04.cs
@@ -1,3 +1,5 @@
+using LaYumba.Functional;
+
 namespace FunctionalProgramming.Exercises.Chapter04;
 
 public static class Solutions
@@ -10,5 +12,15 @@ public static class Solutions
     public static IDictionary<TKey, TResult> Map<TKey, TValue, TResult>(this IDictionary<TKey, TValue> d, Func<TValue, TResult> f) where TKey : notnull
     {
         return new Dictionary<TKey, TResult>(d.Select(pair => KeyValuePair.Create(pair.Key, f(pair.Value))));
+    }
+
+    public static Option<TOut> Map<TIn, TOut>(this Option<TIn> o, Func<TIn, TOut> f)
+    {
+        return o.Bind(x => F.Some(f(x)));
+    }
+
+    public static IEnumerable<TOut> Map<TIn, TOut>(this IEnumerable<TIn> e, Func<TIn, TOut> f)
+    {
+        return e.Bind(x => new[] { f(x) });
     }
 }
