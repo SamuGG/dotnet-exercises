afd1f3cd910bc4214e7c7cd6c5a4dce45bf2541d
SamuGG <SamuGG@users.noreply.github.com>
2023-02-05 19:53:37 +0000

    feat: ApplyR

--- src/Chapter07.cs
+++ src/Chapter07.cs
@@ -1,7 +1,15 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace FunctionalProgramming.Exercises.Chapter07;
 
 public static class Solutions
 {
     public static Func<int, int, int> Remainder =>
         (int a, int b) => a % b;
+
+
+    // ApplyR: (((T1, T2) -> R), T2) -> (T1 -> R)
+    [SuppressMessage("Naming", "CA1715: Identifiers should have correct prefix")]
+    public static Func<T1, R> ApplyR<T1, T2, R>(this Func<T1, T2, R> f, T2 t2) =>
+        t1 => f(t1, t2);
 }
