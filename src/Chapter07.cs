07d46e7b1f3d93388deac00cbad6ea3642d789ec
SamuGG <SamuGG@users.noreply.github.com>
2023-02-05 20:35:02 +0000

    feat: PhoneNumber factory

--- src/Chapter07.cs
+++ src/Chapter07.cs
@@ -16,4 +16,43 @@ public static class Solutions
     [SuppressMessage("Naming", "CA1715: Identifiers should have correct prefix")]
     public static Func<T1, T2, R> ApplyR<T1, T2, T3, R>(this Func<T1, T2, T3, R> f, T3 t3) =>
         (t1, t2) => f(t1, t2, t3);
+
+    [SuppressMessage("Design", "CA1034: Nested types should not be visible")]
+    public class PhoneNumber
+    {
+        public NumberType Type { get; }
+        public CountryCode Code { get; }
+        public string Number { get; }
+
+        public PhoneNumber(NumberType type, CountryCode code, string number)
+        {
+            Type = type;
+            Code = code;
+            Number = number;
+        }
+    }
+
+    public enum NumberType
+    {
+        Home,
+        Mobile
+    }
+
+    [SuppressMessage("Design", "CA1034: Nested types should not be visible")]
+    [SuppressMessage("Usage", "CA2225: Operator overloads have named alternates")]
+    public sealed class CountryCode
+    {
+        public string Value { get; }
+
+        public CountryCode(string value) => Value = value;
+
+        public override string ToString() => Value;
+
+        public static implicit operator CountryCode(string value) => new(value);
+        public static implicit operator string([NotNull] CountryCode code) => code.Value;
+    }
+
+    // Factory: (NumberType, CountryCode, string) -> PhoneNumber
+    public static Func<NumberType, CountryCode, string, PhoneNumber> PhoneNumberFactory =>
+        (type, code, number) => new(type, code, number);
 }
