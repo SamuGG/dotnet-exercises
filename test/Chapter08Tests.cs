7497b08b2f2b8f85700b00b90043ba2b0172c8be
SamuGG <SamuGG@users.noreply.github.com>
2023-02-12 17:29:55 +0000

    test: Apply for Either tests

---
+++ test/Chapter08Tests.cs
@@ -0,0 +1,48 @@
+using FunctionalProgramming.Exercises.Chapter08;
+using F = LaYumba.Functional.F;
+
+namespace FunctionalProgramming.Exercises.Tests.Chapter08;
+
+public class SolutionsTests
+{
+    [Fact]
+    public void ApplyEitherReturnsLeftWhenNoFunction()
+    {
+        const string stateWhenNofunction = "No function";
+        LaYumba.Functional.Either<string, Func<int, double>> noFunction = F.Left(stateWhenNofunction);
+        LaYumba.Functional.Either<string, int> valueToApply = F.Right(5);
+
+        LaYumba.Functional.Either<string, double> actual = noFunction.Apply(valueToApply);
+
+        actual.Match(
+            left => Assert.Equal(stateWhenNofunction, left),
+            right => Assert.Fail("Expected an Either with left state but found right state"));
+    }
+
+    [Fact]
+    public void ApplyEitherReturnsLeftWhenNoValue()
+    {
+        const string stateWhenNoValue = "No value";
+        LaYumba.Functional.Either<string, Func<int, double>> someFunction = F.Right<Func<int, double>>(i => i / 3);
+        LaYumba.Functional.Either<string, int> noValue = F.Left(stateWhenNoValue);
+
+        LaYumba.Functional.Either<string, double> actual = someFunction.Apply(noValue);
+
+        actual.Match(
+            left => Assert.Equal(stateWhenNoValue, left),
+            right => Assert.Fail("Expected an Either with left state but found right state"));
+    }
+
+    [Fact]
+    public void ApplyEitherReturnsRightProvidedFunctionAndValue()
+    {
+        LaYumba.Functional.Either<string, Func<int, double>> someFunction = F.Right<Func<int, double>>(i => i / 3);
+        LaYumba.Functional.Either<string, int> someValue = F.Right(27);
+
+        LaYumba.Functional.Either<string, double> actual = someFunction.Apply(someValue);
+
+        actual.Match(
+            left => Assert.Fail("Expected an Either with right state but found left state"),
+            right => Assert.Equal(9, right));
+    }
+}
