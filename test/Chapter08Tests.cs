8cb43542c40ccfbc6a42b56716a0ddc251e4e174
SamuGG <SamuGG@users.noreply.github.com>
2023-02-12 17:40:59 +0000

    test: Apply for Exceptional tests

--- test/Chapter08Tests.cs
+++ test/Chapter08Tests.cs
@@ -45,4 +45,43 @@ public class SolutionsTests
             left => Assert.Fail("Expected an Either with right state but found left state"),
             right => Assert.Equal(9, right));
     }
+
+    [Fact]
+    public void ApplyExceptionalReturnsLeftWhenNoFunction()
+    {
+        LaYumba.Functional.Exceptional<Func<int, double>> throwingFunction = new NotImplementedException();
+        LaYumba.Functional.Exceptional<int> valueToApply = 25;
+
+        LaYumba.Functional.Exceptional<double> actual = throwingFunction.Apply(valueToApply);
+
+        actual.Match(
+            ex => Assert.IsType<NotImplementedException>(ex),
+            value => Assert.Fail("Expected an exception but found some value instead"));
+    }
+
+    [Fact]
+    public void ApplyExceptionalReturnsLeftWhenNoValue()
+    {
+        LaYumba.Functional.Exceptional<Func<int, double>> someFunction = new Func<int, double>(i => i / 3);
+        LaYumba.Functional.Exceptional<int> exceptionValue = new NotImplementedException();
+
+        LaYumba.Functional.Exceptional<double> actual = someFunction.Apply(exceptionValue);
+
+        actual.Match(
+            ex => Assert.IsType<NotImplementedException>(ex),
+            value => Assert.Fail("Expected an exception but found some value instead"));
+    }
+
+    [Fact]
+    public void ApplyExceptionalReturnsRightProvidedFunctionAndValue()
+    {
+        LaYumba.Functional.Exceptional<Func<int, double>> someFunction = new Func<int, double>(i => i / 3);
+        LaYumba.Functional.Exceptional<int> someValue = 30;
+
+        LaYumba.Functional.Exceptional<double> actual = someFunction.Apply(someValue);
+
+        actual.Match(
+            ex => Assert.Fail("Expected some value but found an exception instead"),
+            right => Assert.Equal(10, right));
+    }
 }
