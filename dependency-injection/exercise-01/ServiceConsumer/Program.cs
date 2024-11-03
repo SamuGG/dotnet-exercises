ef2eb021533ac231ea47685b0795a1a145f63b38
SamuGG <SamuGG@users.noreply.github.com>
2024-11-03 20:36:09 +0000

    chore: Setup DI exercise

---
+++ dependency-injection/exercise-01/ServiceConsumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+services.AddSingleton<IConsoleWriter, ConsoleWriter>();
+var serviceProvider = services.BuildServiceProvider();
+var service = serviceProvider.GetRequiredService<IConsoleWriter>();
+service.WriteLine("Hello");
+
+interface IConsoleWriter
+{
+    void WriteLine(string message);
+}
+
+sealed class ConsoleWriter : IConsoleWriter
+{
+    public void WriteLine(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
