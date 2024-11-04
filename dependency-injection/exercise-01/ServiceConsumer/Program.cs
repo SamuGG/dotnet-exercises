11939123be1dbffe50626beb4d7891a0227da089
SamuGG <SamuGG@users.noreply.github.com>
2024-11-04 22:34:30 +0000

    feat: Use case for transitive dependencies

--- dependency-injection/exercise-01/ServiceConsumer/Program.cs
+++ dependency-injection/exercise-01/ServiceConsumer/Program.cs
@@ -4,9 +4,10 @@ using ServiceContainer;
 var services = new ServiceCollection();
 
 // Flip these lines to register a service using different overloads
-services.AddSingleton<IConsoleWriter, ConsoleWriter>();
-// services.AddSingleton<ConsoleWriter>();
-// services.AddSingleton(serviceProvider => new ConsoleWriter());
+services.AddSingleton<IConsoleWriter, ConsoleWriter>();         // By interface
+// services.AddSingleton<ConsoleWriter>();                         // By implementation
+// services.AddSingleton(serviceProvider => new ConsoleWriter());  // By factory
+// services.AddSingleton(serviceProvider => new ConsoleWriter(serviceProvider.GetService<IGuidProvider>())); // With transitive dependencies
 
 // Flip these lines to see different results
 services.AddSingleton<IGuidProvider, GuidProvider>();
@@ -26,3 +27,4 @@ if (guidGenerator1 is null || guidGenerator2 is null)
 
 writerService.WriteLine(guidGenerator1.Value.ToString());
 writerService.WriteLine(guidGenerator2.Value.ToString());
+writerService.WriteGuid();
--- dependency-injection/exercise-01/ServiceConsumer/Services/ConsoleWriter.cs
+++ dependency-injection/exercise-01/ServiceConsumer/Services/ConsoleWriter.cs
@@ -3,12 +3,32 @@ namespace ServiceConsumer.Services;
 interface IConsoleWriter
 {
     void WriteLine(string message);
+    void WriteGuid();
 }
 
 public sealed class ConsoleWriter : IConsoleWriter
 {
+    private readonly IGuidProvider? _guidProvider;
+
+    public ConsoleWriter()
+    {
+    }
+
+    public ConsoleWriter(IGuidProvider? guidProvider)
+    {
+        _guidProvider = guidProvider;
+    }
+
     public void WriteLine(string message)
     {
         Console.WriteLine(message);
+    }
+
+    public void WriteGuid()
+    {
+        if (_guidProvider is null)
+            return;
+
+        Console.WriteLine(_guidProvider.Value);
     }
 }
